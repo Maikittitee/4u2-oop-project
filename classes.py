a9dc4a0133d467b1512a5c9060f693cd7e546130
ktunchar <maikittitee1615@gmail.com>
2023-03-30 17:10:34 +0700

    feat[method] add_to_cart's method implement

--- classes.py
+++ classes.py
@@ -6,7 +6,7 @@
 #    By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/21 23:17:03 by ktunchar          #+#    #+#              #
-#    Updated: 2023/03/27 23:27:33 by ktunchar         ###   ########.fr        #
+#    Updated: 2023/03/30 17:10:01 by ktunchar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,6 +17,16 @@ from datetime import datetime
 import time
 from enum import Enum
 
+class Colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 ################################################################
 # --------------------- MANAGEMENT CLASS --------------------- #
@@ -42,27 +52,28 @@ class ProductCatalog:
 
 class	Product:
 	def __init__(self, product_id : str, product_name:str, product_price:int, product_description: str, product_detail : str, product_type : list, product_stock : int, product_specify : str):
-		self.product_id =  product_id
-		self.product_name = product_name
-		self.product_price = product_price
-		self.product_description = product_description
-		self.product_detail = product_detail
-		self.product_type = product_type
-		self.product_stock = product_stock
-		self.product_specify = product_specify
+		self.id =  product_id
+		self.name = product_name
+		self.price = product_price
+		self.description = product_description
+		self.detail = product_detail
+		self.type = product_type
+		self.stock = product_stock
+		self.specify = product_specify
+
+class	Item:
+	def	__init__ (self, product, quanity, promotion):
+		self.product = product
+		self.quanity = quanity
+		self.promotion = promotion # Composition Promotion
 
 class	Promotion:
-	def	__init__ (self, product:list,date_start, date_end, discount):
+	def	__init__ (self, product_list:list,date_start, date_end, discount):
 		self.date_start = date_start
 		self.date_end = date_end
 		self.discount = discount
-		self.product = product # COMPOSITION Product
+		self.products = product_list # COMPOSITION Product
 
-class	Item:
-	def	__init__ (self, product, quanity):
-		self.quanity = quanity
-		self.avaiable_promotion = [] # Composition Promotion
-		self.product = product
 
 
 
@@ -107,11 +118,22 @@ class AuthenticationUser(Customer):
 #########################################################
 
 class	ShoppingCart:
-	def __init__ (self, cart_id):
+	def __init__ (self, cart_id, promotions):
 		self.cart_id = cart_id
 		self.total_amount = 0
-		self.promotion = shop.promotions # Association Promotion (but it accully need to keep ALL Promotion then it's better if we use Shop)
+		self.promotions = promotions # Association Promotion (but it accully need to keep ALL Promotion then it's better if we use Shop)
 		self.items = [] # Aggretion Item
+
+	def get_promotion(self, product):
+		for promotion in self.promotions:
+			for avaiable_product in promotion.products :
+				if (avaiable_product is product):
+					return (self.promotion)
+		return (None)
+
+	def	add_to_cart(self, product, quantity):
+		item = Item(product, quantity, self.get_promotion(product))
+		self.items.append(item)
 
 class	Favorite:
 	def	__init__ (self):
@@ -164,6 +186,13 @@ class	OrderStatus(Enum):
 # time.sleep(10)
 # print(product.last_update)
 
+product_1 = Product("65010030","Jelly Tint", 259, "Magic Lib Tint", "This is detail\nThis lib made by angle that came from heaven\nHave been sell For 10 year",["Lib"],9,"#07")
+product_2 = Product("65010134","EST. HARDDER 2", 229, "nothing here", "This is another detaikl",["Lib"],1,"#31")
 promo = Promotion(["65010030"],"1/1/2022","31/12/2023", 39)
 shop.promotions.append(promo)
-cart = ShoppingCart("sh11111")
+cart = ShoppingCart("fuck",shop.promotions)
+cart.add_to_cart(product_1,3)
+# cart.add_to_cart(product_2,2)
+
+for item in cart.items:
+	print(item.product.name)
