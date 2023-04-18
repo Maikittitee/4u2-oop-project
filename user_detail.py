d7ba5de1a0e3fe116f5a9d40e3f82ce8ad11a5e6
jjxwaaa <110412162+jjxwaaa@users.noreply.github.com>
2023-04-18 10:52:34 +0700

    feat : user_detail

--- user_detail.py
+++ user_detail.py
@@ -1,5 +1,13 @@
 #from typing import Union
-#from fastapi import FastAPI
+from fastapi import FastAPI
+
+class Shop:
+	def __init__(self):
+		self.product_catalog = [] #AGRET ProductCatalog
+		self.users = [] #AGGRESION User
+		self.promotions = [] # AGGRESTION Promotion
+
+shop = Shop()
 
 class User:
 	def __init__ (self, user_id, email, name):
@@ -20,7 +28,8 @@ class Admin(User):
 
 class Customer(User):
 	def __init__ (self):
-		self.shopping_cart = ShoppingCart('SC' + user_id) # Association ShoppingCart 
+		User.__init__(self,"1","email","Jiwa")
+		# self.shopping_cart = ShoppingCart('SC' + user_id) # Association ShoppingCart 
 
 class Guest(Customer):
 	def	__init__ (self):
@@ -28,6 +37,7 @@ class Guest(Customer):
 
 class AuthenticationUser(Customer):
 	def	__init__ (self, address):
+		Customer.__init__(self)
 		self.address = address
 		self.order = [] # Aggretion Order
 
@@ -42,10 +52,21 @@ class AuthenticationUser(Customer):
 		}
 		return (view_user_dict)
 
+
 user_3 = AuthenticationUser("sssssss")
-user_3.user_id = "65010437"
+user_3.user_id = "1"
 user_3.email = "haha@gmail.com"
 user_3.name = "6dsfhifhr"
 user_3.address = "sygifkmsl"
+shop.users.append(user_3)
+
+app = FastAPI()
+
+@app.get("/users/{user_id}")
+def get_user(user_id:str):
+    for user in shop.users:
+        if (user.user_id == user_id):
+            return (user.return_user_detail())
+
 
 print(user_3.return_user_detail())
